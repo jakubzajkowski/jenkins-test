cbf8af3de8534b82d8d25e7c31a02b49cd3b6124
Jakub Zajkowski <jakudjan123@gmail.com>
2024-01-29 14:53:27 +0100

    ref: changed package.json

--- cypress/e2e/test_spec.cy.js
+++ cypress/e2e/test_spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test', () => {
     it('should do something', () => {
-        cy.visit('jenkins-test.infinityfreeapp.com');
+        cy.visit('http://jenkins-test.infinityfreeapp.com/');
         cy.contains('Test Page')
     });
 });
