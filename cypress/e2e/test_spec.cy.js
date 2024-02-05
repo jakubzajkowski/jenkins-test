f26ce0c8559bbc10c9e72634eb6d18ed388bf88f
Jakub Zajkowski <jakudjan123@gmail.com>
2024-02-05 12:16:03 +0100

    feat: github action good

--- cypress/e2e/test_spec.cy.js
+++ cypress/e2e/test_spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test', () => {
     it('should do something', () => {
         cy.visit('http://jenkins-test.infinityfreeapp.com/');
-        cy.contains('Nowy Test zmiana')
+        cy.contains('hello world')
     });
 });
